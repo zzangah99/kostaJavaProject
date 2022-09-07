1a387bf72db9f5b692256257bd9516af79a403ce
YongSik <dyrlqhffo@daum.net>
2022-09-07 13:16:20 +0900

    일단 한 거 커밋

--- src/mvc/dao/CategoryDAOImpl.java
+++ src/mvc/dao/CategoryDAOImpl.java
@@ -1,45 +1,9 @@
 package mvc.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
 
-import mvc.dto.Category;
+public class CategoryDAOImpl implements CategoryDAO{
 
-import mvc.util.DbUtil;
-
-public class CategoryDAOImpl implements CategoryDAO() {
-	private Properties proFile = DbUtil.getProfile();
-	Category category = new Category();
 	
-	
-	@Override
-	public List<Category> selectCategory() throws SQLException {
-		Connection con = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		List<Category> categoryList = new ArrayList<Category>();
-		String sql = proFile.getProperty("goodsCategory.selectAll"); //select * from goods_category
-
-		try {
-			con = DbUtil.getConnection();
-			ps = con.prepareStatement(sql);
-			rs = ps.executeQuery();
-
-			while (rs.next()) {
-			    Category category = new Category(rs.getInt(1), rs.getString(2));
-				categoryList.add(category);
-			}
-		} finally {
-			DbUtil.dbClose(con, ps, rs);
-		}
-		return categoryList;
-
-	}
 	
 	
 }
