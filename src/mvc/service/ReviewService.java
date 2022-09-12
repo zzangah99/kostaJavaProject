9d03fbf7ea328b8bfc92d183dd15f17c6c4d92e6
YongSik <dyrlqhffo@daum.net>
2022-09-12 14:12:02 +0900

    no message

--- src/mvc/service/ReviewService.java
+++ src/mvc/service/ReviewService.java
@@ -10,11 +10,10 @@ import mvc.dto.Review;
 public class ReviewService {
 	private ReviewDAO reviewDAO = new ReviewDAOImpl();
 	
-	public void reviewService(Review review) throws SQLException{
-		if( reviewDAO.writeReview(review)== 0) throw new SQLException("리뷰가 등록 되지 않았습니다.");
+
 		
 	}
 	
 	
 	
-}
+
